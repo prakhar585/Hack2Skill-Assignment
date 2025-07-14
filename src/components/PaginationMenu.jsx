5b5506d01bbbc9491536e6b449d7efd11d68bd69
unknown <prakharchaturvedi9860@gmail.com>
2025-07-14 14:16:45 +0530

    font adjustment for pagination menu

--- src/components/PaginationMenu.jsx
+++ src/components/PaginationMenu.jsx
@@ -4,11 +4,11 @@ const PaginationMenu = ({currentPage, handlePreviousPage, handleNextPage}) => {
   const totalPages = Math.round(totalProducts/10);
 
   return (
-    <div className='flex items-center justify-center py-3 bg-indigo-50'>
-      <button className=' bg-indigo-600 text-white px-3 py-1 rounded-md desktop:text-md' onClick={handlePreviousPage} disabled={currentPage === 1}>Prev</button>
-      <button className='px-3 mx-1 bg-white rounded-md py-1 desktop:text-md'>Page {currentPage} of {totalPages}</button>
+    <div className='flex items-center justify-center py-3 bg-indigo-50 text-sm'>
+      <button className=' bg-indigo-600 text-xs text-white px-3 py-1 rounded-md desktop:text-md' onClick={handlePreviousPage} disabled={currentPage === 1}>Prev</button>
+      <button className='px-3 mx-1 text-xs bg-white rounded-md py-1 desktop:text-sm'>Page {currentPage} of {totalPages}</button>
     
-      <button className='px-2 bg-indigo-600 text-white px-3 py-1 rounded-md desktop:text-md' onClick={handleNextPage}>Next</button>
+      <button className='px-2 bg-indigo-600 text-xs text-white px-3 py-1 rounded-md desktop:text-md' onClick={handleNextPage}>Next</button>
     </div>
   )
 }
